974af3f7bdee879197b96cd6e693490e4faede0b
Daniel Westbrook <web@pixelatedawesome.com>
2026-06-19 00:41:44 -0500

    it's slightly less stupid now

--- hashtree/__init__.py
+++ hashtree/__init__.py
@@ -1,6 +1,10 @@
 import argparse
 import hashlib
+import sys
 from pathlib import Path
+
+from typing import Tuple, TextIO
+
 
 __version__ = '0.0.1'
 
@@ -16,38 +20,43 @@ def setup_argparse() -> argparse.ArgumentParser:
 def run() -> None:
     args: argparse.Namespace = setup_argparse().parse_args()
     for path in args.paths:
-        hash_tree(Path(path))
+        hash_tree(Path(path), sys.stdout)
 
 
-def hash_tree(path: Path) -> str:
+def hash_tree(path: Path, stream: TextIO) -> str:
     stack: list[Path] = []
     todo_list: list[Path] = [path]
     while todo_list:
         directory: Path = todo_list.pop()
-        print("┃"*(len(stack)-1) + "┣" + str(directory.stem))
+        stream.write("┃" * (len(stack) - 1) + "┣" + str(directory.stem) + "\n")
         while stack and directory.parent != stack[-1]:
             stack.pop()
         stack.append(directory)
 
-        subdirectories: list[Path] = []
-        files: list[Path] = []
-        for item in directory.iterdir():
-            if item.is_symlink():
-                raise NotImplementedError("symlinks not implemented")
-            elif item.is_dir():
-                subdirectories.append(item)
-            elif item.is_file():
-                files.append(item)
-            else:
-                raise NotImplementedError("Huh???")
-
-        subdirectories.sort()
-        files.sort()
+        files, subdirectories = sort_contents(directory)
 
         for f in files:
-            print("┃"*(len(stack)-1) + "┣" + str(f.stem) + ": " + hash_file(f))
-        for subdirectory in subdirectories:
-            todo_list.append(subdirectory)
+            stream.write("┃"*(len(stack)-1) + "┣" + str(f.stem))
+            stream.write(": " + hash_file(f) + "\n")
+        todo_list.extend(subdirectories)
+
+
+def sort_contents(directory: Path) -> Tuple[list[Path], list[Path]]:
+    subdirectories: list[Path] = []
+    files: list[Path] = []
+    for item in directory.iterdir():
+        if item.is_symlink():
+            raise NotImplementedError("symlinks not implemented")
+        elif item.is_dir():
+            subdirectories.append(item)
+        elif item.is_file():
+            files.append(item)
+        else:
+            raise NotImplementedError("Huh???")
+
+    subdirectories.sort()
+    files.sort()
+    return files, subdirectories
 
 
 def hash_file(path: Path) -> str:
